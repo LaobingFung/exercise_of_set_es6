216297cf507291a57b5da9080c4785eaad21fb6e
LaoBing_Fung <FLB_B401@163.com>
2018-12-28 20:35:28 +0800

    get intersection of 2 array

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  return collection_b.filter((curItem) => collection_a.includes(curItem));
 }
 
 module.exports = get_intersection;
