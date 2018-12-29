0aaae4f6875c860c9f45a53853776f4432229376
LaoBing_Fung <FLB_B401@163.com>
2018-12-29 12:36:09 +0800

    get letter interval

--- main/collectionOperator/get_letter_interval.js
+++ main/collectionOperator/get_letter_interval.js
@@ -1,7 +1,29 @@
 'use strict';
 
+const ALPHANUMBER = {
+    '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e',
+    '6': 'f', '7': 'g', '8': 'h', '9': 'i', '10': 'j',
+    '11': 'k', '12': 'l', '13': 'm', '14': 'n', '15': 'o',
+    '16': 'p', '17': 'q', '18': 'r', '19': 's', '20': 't',
+    '21': 'u', '22': 'v', '23': 'w', '24': 'x', '25': 'y', '26': 'z'
+}
+
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  let alphaArr = get_integer_interval(number_a, number_b);
+  return alphaArr.map((curItem) => {
+    return ALPHANUMBER[curItem];
+  });
+}
+
+function get_integer_interval(numberA, numberB) {
+  let arr = new Array();
+  let interval = numberA > numberB ? -1 : 1;
+  while (numberA !== numberB) {
+    arr.push(numberA);
+    numberA += interval;
+  }
+  arr.push(numberA);
+  return arr;
 }
 
 module.exports = get_letter_interval;
