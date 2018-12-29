c688ecb0a84194b67660c7e23f222c893d48455d
LaoBing_Fung <FLB_B401@163.com>
2018-12-29 15:55:09 +0800

    convert double dimensional array to one

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  return collection.reduce((acc, curItem) => {
+    return acc = acc.concat(curItem);
+  }, []);
 }
 
 module.exports = double_to_one;
