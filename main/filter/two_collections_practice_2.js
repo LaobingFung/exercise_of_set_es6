a388df536897053bd4d628e3fa0d2eb9c996b9cd
LaoBing_Fung <FLB_B401@163.com>
2018-12-29 15:32:48 +0800

    get incompatible elements of 2 array

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+  return collection_a.filter((curItem) => !collection_b.includes(curItem));
 }
 
 module.exports = choose_no_common_elements;
