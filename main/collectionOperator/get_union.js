0c4545b2ec06c48ff173bc8e9bad2ac801e955f4
LaoBing_Fung <FLB_B401@163.com>
2018-12-29 12:22:23 +0800

    get union of 2 array

--- main/collectionOperator/get_union.js
+++ main/collectionOperator/get_union.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+  let union = collection_a.slice(0);
+  collection_b.forEach((curItem) => {
+    if (!union.includes(curItem)) {
+      union.push(curItem);
+    }
+  });
+  return union;
 }
-
 module.exports = get_union;
-
