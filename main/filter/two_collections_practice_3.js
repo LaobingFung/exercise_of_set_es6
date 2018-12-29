6d8697864b9c5abfd6f95ab5bd48fcbcadfbc795
LaoBing_Fung <FLB_B401@163.com>
2018-12-29 15:48:45 +0800

    get element of collection a that divisible by element of collection b

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+  return collection_a.filter((curA) => {
+    return collection_b.some((curB) => curA % curB === 0)
+  });
 }
-
 module.exports = choose_divisible_integer;
