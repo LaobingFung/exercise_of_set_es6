7d3d9dd0c9dae08a1d1025d7e36b2b1b10f97c02
LaoBing_Fung <FLB_B401@163.com>
2018-12-29 15:25:09 +0800

    count element in an array

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //在这里写入代码
+  let noRepeat = new Set(collection);
+  let counter = {};
+  noRepeat.forEach((curItem) => {
+    counter[curItem] = 0;
+  })
+  collection.forEach((curItem) => counter[curItem] += 1);
+  return counter;
 }
 
 module.exports = grouping_count;
