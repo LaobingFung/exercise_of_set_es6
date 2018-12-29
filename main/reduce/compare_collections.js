7274e1d1127d0482a8ac842677bbe85eb7f0c31b
LaoBing_Fung <FLB_B401@163.com>
2018-12-29 16:43:58 +0800

    compare two collections

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+  return collection_a.reduce((equal, curItem, curI) => {
+    return equal && (curItem === collection_b[curI])
+  }, true)
 }
 
 module.exports = compare_collections;
