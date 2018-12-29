892101fa2a6567fba2a8fe0ca332894160c29178
LaoBing_Fung <FLB_B401@163.com>
2018-12-29 15:59:19 +0800

    flat double dimensional array and filter incompitable element

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  let one_dime = collection.reduce((acc, curItem) => {
+    return acc = acc.concat(curItem);
+  }, [])
+  return one_dime.filter((curItem, curI) => curI === one_dime.indexOf(curItem));
 }
 
 module.exports = double_to_one;
