cd76dbdad8437d175d0e00cc8ba93191eea117c2
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 19:45:28 +0800

    calculate average of odd subscript elements

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,13 @@
 'use strict';
-var calculate_average = function(collection){
 
+function computAverage(collection) {
+  let sum = collection.reduce((acc, curItem) => {
+    return acc += curItem;
+  }, 0);
+  return sum / collection.length;
+}
+var calculate_average = function(collection) {
+  let oddSubscript = collection.filter((curItem, curI) => curI % 2 === 1);
+  return computAverage(oddSubscript);
 };
 module.exports = calculate_average;
