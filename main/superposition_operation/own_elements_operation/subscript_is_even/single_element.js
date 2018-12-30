ae2408cb9b7c132a120aff3c3aea758090eea812
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 19:59:09 +0800

    collect the single elements

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,6 @@
 'use strict';
-var single_element = function(collection){
-
+var single_element = function(collection) {
+  let oddSubscript = collection.filter((curItem, curI) => curI % 2 === 1);
+  return oddSubscript.filter((curItem) => oddSubscript.indexOf(curItem) === oddSubscript.lastIndexOf(curItem));
 };
 module.exports = single_element;
