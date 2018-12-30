0f133c387a465f3dc3cb6586c0c6f3f337467fdf
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 15:45:41 +0800

    collect same elements

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,5 +1,6 @@
-function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
-}
+'use strict';
 
+function collect_same_elements(collection_a, collection_b) {
+  return collection_a.filter((curA) => collection_b.includes(curA));
+}
 module.exports = collect_same_elements;
