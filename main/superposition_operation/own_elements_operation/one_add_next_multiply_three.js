49686186fdb6b792cf444be0cd3116947f467d56
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 18:45:49 +0800

    map to three times of sum of one and next

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,8 @@
 'use strict';
-function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+
+function one_add_next_multiply_three(collection) {
+  let mapArr = collection.map((curItem, curI, arr) => (curItem + arr[curI + 1]) * 3);
+  mapArr.pop();
+  return mapArr;
 }
 module.exports = one_add_next_multiply_three;
