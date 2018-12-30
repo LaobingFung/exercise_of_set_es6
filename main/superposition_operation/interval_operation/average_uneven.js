ddfba3bad80724200f0cb3bce5bbff9c13d91f64
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 18:34:29 +0800

    compute average of odd

--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function average_uneven(collection) {
-
-  //在这里写入代码
+  let oddArr = collection.filter((curItem) => curItem % 2 === 1);
+  let sumOfOdd = oddArr.reduce((acc, curItem) => acc += curItem, 0);
+  return sumOfOdd / oddArr.length;
 }
-
 module.exports = average_uneven;
