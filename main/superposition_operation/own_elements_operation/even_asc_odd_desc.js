1fc330637d4be50e79f72b454cc665220ff01898
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 18:41:13 +0800

    sort odd desc and even asc then concat odd and even array

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,9 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
-
+var even_asc_odd_desc = function(collection) {
+  let oddArr = collection.filter((curItem) => curItem % 2 === 1);
+  let evenArr = collection.filter((curItem) => curItem % 2 === 0);
+  evenArr.sort((a, b) => a - b);
+  oddArr.sort((a, b) => b - a);
+  return evenArr.concat(oddArr);
 };
 module.exports = even_asc_odd_desc;
