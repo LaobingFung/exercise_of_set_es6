a1b79f5e4e1c77c27676309c6d2e0c664226eb93
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 19:53:16 +0800

    test if an element is exist

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,6 @@
 'use strict';
-var is_exist_element = function(collection,element){
-
+var is_exist_element = function(collection, element) {
+  let evenSubscript = collection.filter((curItem, curI) => curI % 2 === 0);
+  return evenSubscript.some((curItem) => curItem === element);
 };
 module.exports = is_exist_element;
