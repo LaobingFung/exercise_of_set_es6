80594e7f4f497ed421bddf9672bcddc3f6cb89ff
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 15:42:22 +0800

    split to zero

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  let arr = [number];
+  while (arr[arr.length - 1] > 0) {
+    number = Math.round((number - interval) * 10) / 10;
+    arr.push(number);
+  }
+  return arr;
 }
 
 module.exports = spilt_to_zero;
