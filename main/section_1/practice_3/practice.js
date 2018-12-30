3f6e1c3a453fd8125877d41fa4d728b7d81fa3b5
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 15:52:32 +0800

    collect same elements

--- main/section_1/practice_3/practice.js
+++ main/section_1/practice_3/practice.js
@@ -1,5 +1,6 @@
-function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
-}
+'use strict';
 
+function collect_same_elements(collection_a, object_b) {
+  return collection_a.filter((curA) => object_b.value.includes(curA));
+}
 module.exports = collect_same_elements;
--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,5 +1,7 @@
-function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
-}
+'use strict';
 
+function collect_same_elements(collection_a, object_b) {
+  let arrA = collection_a.map((curObj) => curObj.key);
+  return arrA.filter((curA) => object_b.value.includes(curA));
+}
 module.exports = collect_same_elements;
