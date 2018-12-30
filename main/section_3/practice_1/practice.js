e3e3b4cd5c007478b53621eae04caa2f1275c57b
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 17:57:36 +0800

    create and update collection

--- main/section_3/practice_1/practice.js
+++ main/section_3/practice_1/practice.js
@@ -1,5 +1,14 @@
-function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
-}
+'use strict';
 
+function create_updated_collection(collection_a, object_b) {
+  let arrA = Array.from(collection_a);
+  object_b.value.forEach((curItem) => {
+    arrA.forEach((curObj) => {
+      if (curObj.key === curItem) {
+        curObj.count -= 1
+      }
+    })
+  });
+  return arrA;
+}
 module.exports = create_updated_collection;
--- main/section_3/practice_4/practice.js
+++ main/section_3/practice_4/practice.js
@@ -1,5 +1,49 @@
-function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+'use strict';
+const KEYREG = /\w/;
+const NUMREG = /[1-9][0-9]*/
+
+function count_same_elements(collection) {
+  let countResult = [];
+  let counter = countElement(collection);
+  for (let object of counter) {
+    countResult.push({
+      key: object[0],
+      count: object[1]
+    });
+  }
+  return countResult;
 }
 
+function countElement(arr) {
+  let counterMap = new Map();
+  arr.forEach((curItem) => {
+    let letter = curItem.match(KEYREG);
+    let num = curItem.match(NUMREG);
+    if (num) {
+      addCountToMap(counterMap, letter[0], parseInt(num[0]))
+    } else {
+      addCountToMap(counterMap, letter[0], 1)
+    }
+  })
+  return counterMap;
+}
+
+function addCountToMap(map, element, count) {
+  if (!map.has(element)) {
+    map.set(element, 0);
+  }
+  map.set(element, map.get(element) + count);
+}
+
+function create_updated_collection(collection_a, object_b) {
+  let arrA = count_same_elements(collection_a);
+  object_b.value.forEach((curItem) => {
+    arrA.forEach((curObj) => {
+      if (curObj.key === curItem) {
+        curObj.count -= Math.floor(curObj.count / 3);
+      }
+    })
+  });
+  return arrA;
+}
 module.exports = create_updated_collection;
