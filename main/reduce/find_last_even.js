235809b35257abb5841ffa11c99b0fbbc8097048
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 15:35:28 +0800

    find last even

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function find_last_even(collection) {
-  //在这里写入代码
+  let arr = Array.from(collection);
+  return arr.reverse().find((curItem) => curItem % 2 === 0);
 }
-
 module.exports = find_last_even;
