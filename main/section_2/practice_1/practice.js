4e0de29370ebc788d9263ab4c89ce36c40265e8b
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 17:07:44 +0800

    count same elements

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,37 @@
+'use strict';
+const KEYREG = /\w/;
+const NUMREG = /[1-9][0-9]*/
+
 function count_same_elements(collection) {
-  //在这里写入代码
+  let countResult = [];
+  let counter = countElement(collection);
+  for (let object of counter) {
+    countResult.push({
+      key: object[0],
+      count: object[1]
+    });
+  }
+  return countResult;
 }
 
+function countElement(arr) {
+  let counterMap = new Map();
+  arr.forEach((curItem) => {
+    let letter = curItem.match(KEYREG);
+    let num = curItem.match(NUMREG);
+    if(num) {
+        addCountToMap(counterMap, letter[0], parseInt(num[0]))
+    } else {
+        addCountToMap(counterMap, letter[0], 1)
+    }
+  })
+  return counterMap;
+}
+
+function addCountToMap(map, element, count) {
+  if (!map.has(element)) {
+    map.set(element, 0);
+  }
+  map.set(element, map.get(element) + count);
+}
 module.exports = count_same_elements;
