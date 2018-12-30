9517b40f8bffb094510f2a9e1d88224007ecb783
LaoBing_Fung <FLB_B401@163.com>
2018-12-30 19:48:43 +0800

    calculate median of odd subscript elements

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,12 @@
 'use strict';
-var calculate_median = function(collection){
 
+function computeMedian(collection) {
+  collection.sort((a, b) => b - a);
+  let medianI = (collection.length - 1) / 2;
+  return (collection[Math.floor(medianI)] + collection[Math.ceil(medianI)]) / 2;
+}
+var calculate_median = function(collection) {
+  let oddSubscript = collection.filter((curItem, curI) => curI % 2 === 1);
+  return computeMedian(oddSubscript);
 };
 module.exports = calculate_median;
